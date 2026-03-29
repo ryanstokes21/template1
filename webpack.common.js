ad021c48b5fd857699cf3f974034513f3a08ae5f
Ryan Stokes <ryanstokescfbre@gmail.com>
2026-03-29 17:50:40 -0500

    add all setup for template

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+import path from 'node:path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(import.meta.dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
